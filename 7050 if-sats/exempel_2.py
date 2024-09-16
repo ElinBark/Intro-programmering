ac073579fceb5c40bf2ee5fbbdbacb3879adfb0b
Elin bark <elin.bark06@skola.stenungsund.se>
2024-09-16 10:18:54 +0200

    klar 7050

---
+++ 7050 if-sats/exempel_2.py
@@ -0,0 +1,7 @@
+tal = int(input("Mata in ett heltal: "))
+if tal >= 0 and tal < 10:
+    print('positivt ental')
+elif tal < 0:
+    print('negativt tal')
+else:
+    print("positivt flersiffrigt tal")
