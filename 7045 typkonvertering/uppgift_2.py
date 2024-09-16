e034ace04b358f86c17bab08000b4d3d1f73bda2
Elin bark <elin.bark06@skola.stenungsund.se>
2024-09-16 10:18:12 +0200

    klar 7045

---
+++ 7045 typkonvertering/uppgift_2.py
@@ -0,0 +1,6 @@
+r = float(input("Ange radien för cikeln: "))
+omkrets = 2*r*3.14
+area = r*r*3.14
+print("omkrets =", omkrets)
+print("area =", area)
+print("Cirkelns omkrets är", omkrets, "och arean är", area)
